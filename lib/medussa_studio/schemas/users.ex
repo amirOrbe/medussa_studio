6335562eafee08df2a7c2e4c3ef330af9fe4f3ef
Amir <67482512+amirOrbe@users.noreply.github.com>
2022-08-25 22:23:56 -0500

    Adding schema for users (#5)
    Co-authored-by: amirOrbe <amir.orbe@bunsan.io>

---
+++ lib/medussa_studio/schemas/users.ex
@@ -0,0 +1,25 @@
+defmodule MedussaStudio.Schemas.Users do
+  @doc """
+  Schema for users
+  """
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  schema "users" do
+    field :age, :integer
+    field :dateOfBirth, :date
+    field :email, :string
+    field :name, :string
+    field :password, :string
+    field :phone, :integer
+
+    timestamps()
+  end
+
+  @doc false
+  def changeset(users, attrs) do
+    users
+    |> cast(attrs, [:name, :email, :dateOfBirth, :password, :age, :phone])
+    |> validate_required([:name, :email, :dateOfBirth, :password, :age, :phone])
+  end
+end
