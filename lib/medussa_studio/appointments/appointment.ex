afd2259d397f4612204976c10725c2d045b018bd
amirOrbe <amir.orbe@bunsan.io>
2022-11-01 12:20:00 -0600

    Adding some styles

--- lib/medussa_studio/appointments/appointment.ex
+++ lib/medussa_studio/appointments/appointment.ex
@@ -2,12 +2,12 @@ defmodule MedussaStudio.Appointments.Appointment do
   use Ecto.Schema
   import Ecto.Changeset
 
-  @fields ~w(date time service status)a
+  @fields ~w(date time service)a
 
   schema "appointment" do
     field :date, :string
     field :service, :string
-    field :status, :string
+    field :status, Ecto.Enum, values: [:Aceptado, :Rechazado, :Espera]
     field :time, :string
     field :user_id, :id
 
