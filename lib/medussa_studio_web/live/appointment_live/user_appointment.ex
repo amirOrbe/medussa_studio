308b0f4f89a8dcf4735ae783d539eca4e25b2c0e
amirOrbe <amir.orbe@bunsan.io>
2022-11-04 23:06:18 -0600

    Saving an appointment correctly and redirect to index

--- lib/medussa_studio_web/live/appointment_live/user_appointment.ex
+++ lib/medussa_studio_web/live/appointment_live/user_appointment.ex
@@ -1,43 +1,40 @@
 defmodule MedussaStudioWeb.AppointmentLive.UserAppointment do
   use MedussaStudioWeb, :live_view
   alias MedussaStudio.Appointments
-  alias MedussaStudio.Repo
-  # alias MedussaStudio.Appointments.Appointment
 
   def mount(params, _, socket) do
     socket =
       assign(socket,
         changeset: Appointments.validate_appointment(%{}),
-        date: correct_map_structure(params)
+        date: json_to_map(params)
       )
 
     {:ok, socket}
   end
 
-  # def handle_event("validate", %{"servicio" => params}, socket) do
-  #   changeset =
-  #     %Appointment{}
-  #     |> Appointment.changeset(params)
-  #     |> Map.put(:action, :insert)
-
-  #   {:noreply, assign(socket, changeset: changeset)}
-  # end
-
   def handle_event("save", %{"appointment" => appointment_attrs}, socket) do
     date = Enum.into(socket.assigns.date, %{})
 
-    appointment_changeset =
+    appointment =
       appointment_attrs
-      |> correct_map_structure()
-      |> Map.new()
+      |> json_to_map()
       |> Map.merge(date)
-      |> Appointments.validate_appointment()
 
-    Repo.insert(appointment_changeset)
-    {:noreply, socket}
+    case Appointments.save_appointment(appointment) do
+      {:ok, _appointment} ->
+        {:noreply,
+         socket
+         |> put_flash(:info, "Cita creada")
+         |> redirect(to: "/")}
+
+      {:error, %Ecto.Changeset{} = changeset} ->
+        {:noreply, assign(socket, changeset: changeset)}
+    end
   end
 
-  def correct_map_structure(params) do
-    Enum.map(params, fn {key, value} -> {String.to_existing_atom(key), value} end)
+  def json_to_map(params) do
+    params
+    |> Enum.map(fn {key, value} -> {String.to_existing_atom(key), value} end)
+    |> Map.new()
   end
 end
