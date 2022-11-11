3986c0296efd1de45e84a58ff172e2d4b408e88f
amirOrbe <amir.orbe@bunsan.io>
2022-11-11 16:30:38 -0600

    Adding live component module for profile

---
+++ lib/medussa_studio_web/live/component_live/profile_component.ex
@@ -0,0 +1,9 @@
+defmodule MedussaStudioWeb.ComponentLive.ProfileComponent do
+  use MedussaStudioWeb, :live_component
+
+  def render(assigns) do
+    ~H"""
+    <div class="hero">HI</div>
+    """
+  end
+end
